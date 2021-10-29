082f5073e235e62c55044c20d980134a2c4fe27e
LXR YOUTUBER AMAL <90690553+LXR-YOUTUBER-AMAL@users.noreply.github.com>
2021-10-29 23:17:50 +0530

    Update banned.py

--- plugins/banned.py
+++ plugins/banned.py
@@ -25,7 +25,7 @@ async def ban_reply(bot, message):
 @Client.on_message(filters.group & disabled_group)
 async def grp_bd(bot, message):
     buttons = [[
-        InlineKeyboardButton('Support', url='https://t.me/EvaMariaSupport')
+        InlineKeyboardButton('SUPPORT 📂', url='https://t.me/malayalammoviesmms')
     ]]
     reply_markup=InlineKeyboardMarkup(buttons)
     vazha = await db.get_chat(message.chat.id)
