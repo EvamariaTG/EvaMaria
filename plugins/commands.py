ab1817fa877ba3c2f6355d453da2523070496c49
MRK-YT <81806065+MRK-YT@users.noreply.github.com>
2021-10-29 22:49:00 +0530

    Bot Username

--- plugins/commands.py
+++ plugins/commands.py
@@ -17,9 +17,11 @@ async def start(client, message):
     if not await db.is_user_exist(message.from_user.id):
         await db.add_user(message.from_user.id, message.from_user.first_name)
         await client.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(message.from_user.id, message.from_user.mention))
+        EVAMARIABOT = await client.get_me()
     if len(message.command) != 2:
+
         buttons = [[
-            InlineKeyboardButton('➕ Add Me To Your Groups ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
+            InlineKeyboardButton('➕ Add Me To Your Groups ➕', url=f'http://t.me/{EVAMARIABOT.username}?startgroup=true')
             ],[
             InlineKeyboardButton('🔍 Search', switch_inline_query_current_chat=''),
             InlineKeyboardButton('🤖 Updates', url='https://t.me/EvaMariaUpdates')
