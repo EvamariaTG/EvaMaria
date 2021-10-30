b7e94ca10a68a6131cf4661de243f835aef0b834
sahanhisharasl <89434202+sahanhisharasl@users.noreply.github.com>
2021-10-30 10:39:21 +0530

    Update misc.py

--- plugins/misc.py
+++ plugins/misc.py
@@ -158,10 +158,10 @@ async def imdb_callback(bot: Client, query: CallbackQuery):
             ]
         ]
     if imdb.get('poster'):
-        await query.message.reply_photo(photo=imdb['poster'], caption=f"\n\n🏷🎬 <b>Title</b>:<a href={imdb['url']}>{imdb.get('title')}</a>\n <b>🎭 Genres : </b>{imdb.get('genres')}\n <b>📆 Year: </b><a href={imdb['url']}/releaseinfo>{imdb.get('year')}</a>\n <b>🌟 Rating:</b> <a href={imdb['url']}/ratings>{imdb.get('rating')}</a> / 10\n <b>✏️ StoryLine:</b> <b><i>{imdb.get('plot')} </i></b> \n \n <b> ‌‌‌‌‎©️ＳＬＯＦＦＩＣＩＡＬ ­ </b>", reply_markup=InlineKeyboardMarkup(btn))
+        await query.message.reply_photo(photo=imdb['poster'], caption=f"🎬 <b>Title</b>:<a href={imdb['url']}>{imdb.get('title')}</a>\n <b>🎭 Genres : </b>{imdb.get('genres')}\n <b>📆 Year: </b><a href={imdb['url']}/releaseinfo>{imdb.get('year')}</a>\n <b>🌟 Rating:</b> <a href={imdb['url']}/ratings>{imdb.get('rating')}</a> / 10\n <b>✏️ StoryLine:</b> <b><i>{imdb.get('plot')} </i></b> \n \n <b> ‌‌‌‌‎©️ＳＬＯＦＦＩＣＩＡＬ ­ </b>", reply_markup=InlineKeyboardMarkup(btn))
         await query.message.delete()
     else:
-        await query.message.edit(f"IMDb Data:\n\n🏷🎬<b> Title :<b> <a href={imdb['url']}>{imdb.get('title')}</a>\n <b>🎭 Genres :</b> {imdb.get('genres')}\n <b>📆 Year: </b><a href={imdb['url']}/releaseinfo>{imdb.get('year')}</a>\n </b>🌟 Rating:</b> <a href={imdb['url']}/ratings>{imdb.get('rating')}</a> / 10\n✏️<b> StoryLine:<b> <b><i>{imdb.get('plot')} </i></b>  \n \n <b> ‌‌‌‌‎©️ＳＬＯＦＦＩＣＩＡＬ ­ </b>", reply_markup=InlineKeyboardMarkup(btn), disable_web_page_preview=True)
+        await query.message.edit(f"🎬<b> Title :<b> <a href={imdb['url']}>{imdb.get('title')}</a>\n <b>🎭 Genres :</b> {imdb.get('genres')}\n <b>📆 Year: </b><a href={imdb['url']}/releaseinfo>{imdb.get('year')}</a>\n </b>🌟 Rating:</b> <a href={imdb['url']}/ratings>{imdb.get('rating')}</a> / 10\n✏️<b> StoryLine:<b> <b><i>{imdb.get('plot')} </i></b>  \n \n <b> ‌‌‌‌‎©️ＳＬＯＦＦＩＣＩＡＬ ­ </b>", reply_markup=InlineKeyboardMarkup(btn), disable_web_page_preview=True)
     await query.answer()
         
 
