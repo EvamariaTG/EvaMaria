6bf570723a04516d8636376b0593d4ae10ea340e
sahanhisharasl <89434202+sahanhisharasl@users.noreply.github.com>
2021-10-31 14:12:24 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -60,28 +60,10 @@ async def start(client, message):
             parse_mode="markdown"
             )
         return
-     if len(message.command) ==2 and message.command[1] in ["subscribe", "error", "okay"]:
-                    buttons = [[
-            InlineKeyboardButton(' Add Me To Your Groups ', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
-            ],[
-            InlineKeyboardButton(' Search', switch_inline_query_current_chat=''),
-            InlineKeyboardButton(' Updates', url='https://t.me/slofficialmain')
-            ],[
-            InlineKeyboardButton(' Help', callback_data='help'),
-            InlineKeyboardButton(' About', callback_data='about')
-        ]]
-        reply_markup = InlineKeyboardMarkup(buttons)
-        await message.reply_photo(
-            photo=random.choice(PICS),
-            caption=script.START_TXT.format(message.from_user.mention),
-            reply_markup=reply_markup,
-            parse_mode='html'
-        )
-        if not await db.is_user_exist(message.from_user.id):
-            await db.add_user(message.from_user.id, message.from_user.first_name)
+     if message.command[1] in ["subscribe", "error", "okay"]:
         return
     file_id = message.command[1]
-    
+    print(file_id)
     files = (await get_file_details(file_id))[0]
     title = files.file_name
     size=get_size(files.file_size)
