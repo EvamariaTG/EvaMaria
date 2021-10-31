cbcb80d476b988a3555a7b210dc48416e5b3183e
Mufaz-TG <92558762+Mufaz-TG@users.noreply.github.com>
2021-10-31 11:32:52 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -40,7 +40,7 @@ class script(object):
 3. Buttons should be properly parsed as markdown format
 
 <b>URL buttons:</b>
-<code>[Button Text](buttonurl:https//t.me/EvaMariaBot)</code>
+<code>[Button Text](buttonurl:https://t.me/EvaMariaBot)</code>
 
 <b>Alert buttons:</b>
 <code>[Button Text](buttonalert:This is an alert message)</code>"""
