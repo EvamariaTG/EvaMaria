4e282726bb8c7e17ffa958ab304ead8f0f72d88e
Mr DudE <90444865+Greatbotmaker@users.noreply.github.com>
2021-11-01 19:30:02 +0530

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -1,7 +1,7 @@
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from pyrogram.errors.exceptions.bad_request_400 import MessageTooLong, PeerIdInvalid
-from info import ADMINS, LOG_CHANNEL, SUPPORT_CHAT
+from info import ADMINS, LOG_CHANNEL, SUPPORT_CHAT, MELCOW_MSG1
 from database.users_chats_db import db
 from database.ia_filterdb import Media
 from utils import get_size, temp
