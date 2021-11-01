a4bbf93679106681aa4b31f889ec67608ea7f400
Rizwan <83484824+hockbhmv@users.noreply.github.com>
2021-11-01 19:37:22 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -22,7 +22,7 @@ async def start(client, message):
             InlineKeyboardButton('➕ Add Me To Your Groups ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
             ],[
             InlineKeyboardButton('🔍 Search', switch_inline_query_current_chat=''),
-            InlineKeyboardButton('🤖 Updates', url=f'{GROUP}')
+            InlineKeyboardButton('🤖 Updates', url=f'{group}')
             ],[
             InlineKeyboardButton('ℹ️ Help', callback_data='help'),
             InlineKeyboardButton('😊 About', callback_data='about')
