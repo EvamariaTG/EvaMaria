31392e4d323fe22b9012058c69fcc54333c33fe0
Mr DudE <90444865+Greatbotmaker@users.noreply.github.com>
2021-11-01 19:32:56 +0530

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -51,7 +51,8 @@ async def save_group(bot, message):
                     await (temp.MELCOW['welcome']).delete()
                 except:
                     pass
-            temp.MELCOW['welcome'] = await message.reply(f"<b>Hey , {u.mention}, Welcome to {message.chat.title}</b>")
+            f_melcow1=MELCOW_MSG1.format()    
+            temp.MELCOW['welcome'] = await message.reply(f"{f_melcow1}")
 
 
 @Client.on_message(filters.command('leave') & filters.user(ADMINS))
