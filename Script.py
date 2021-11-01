fc0f752a1acc8d328df4f0da4e21c81e6e785a8d
Rizwan <83484824+hockbhmv@users.noreply.github.com>
2021-11-01 19:02:11 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -36,7 +36,7 @@ class script(object):
 
 <b>NOTE:</b>
 1. Telegram will not allows you to send buttons without any content, so content is mandatory.
-2. Eva Maria supports buttons with any telegram media type.
+2. m.d_filter_bot supports buttons with any telegram media type.
 3. Buttons should be properly parsed as markdown format
 
 <b>URL buttons:</b>
