b45ece89293ad1fef12bf36dfd58e509443e27a9
Rizwan <83484824+hockbhmv@users.noreply.github.com>
2021-11-04 00:33:16 +0530

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,12 @@
+if [ -z $UPSTREAM_REPO ]
+then
+  echo "Cloning main Repository"
+  git clone https://github.com/EvamariaTG/EvaMaria.git /EvaMaria
+else
+  echo "Cloning Custom Repo from $UPSTREAM_REPO "
+  git clone $UPSTREAM_REPO /EvaMaria
+fi
+cd /EvaMaria
+pip3 install -U -r requirements.txt
+echo "Starting Bot...."
+python3 bot.py
