3b77735a3bf6df5ababfcdbdb6446c74d39f1460
saikatwtf <84172414+saikatwtf@users.noreply.github.com>
2021-11-04 15:09:52 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -73,7 +73,7 @@ async def next_page(bot, query):
 
     ident, req, key, offset = query.data.split("_")
     if int(req) not in [query.from_user.id, 0]:
-        return await query.answer("oKda", show_alert=True)
+        return await query.answer("এটা তোমার জন্য নয় সোনা👀", show_alert=True)
     try:
         offset = int(offset)
     except:
