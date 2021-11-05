38b05238a18fc818147aae6a718b0f3c85a42601
SUBIN <64341611+subinps@users.noreply.github.com>
2021-11-05 21:32:29 +0530

    fix a fix

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -716,7 +716,7 @@ async def advantage_spell_chok(msg):
                 )
             ] for k, movie in enumerate(movielist)]
     btn.append([InlineKeyboardButton(text="Close", callback_data=f'spolling#{user}#close_spellcheck')])
-    await msg.reply('I couldn't find anything related to that\nDid you mean any one of these?', reply_markup=InlineKeyboardMarkup(btn))
+    await msg.reply("I couldn't find anything related to that\nDid you mean any one of these?", reply_markup=InlineKeyboardMarkup(btn))
     
 
 
