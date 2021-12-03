6f8c129d9c4c415cac7f5266661c6954caf7646a
Rithin2277 <92090513+Rithin2277@users.noreply.github.com>
2021-12-03 16:47:06 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -11,14 +11,14 @@ class script(object):
 ✯ 𝙱𝙾𝚃 𝚂𝙴𝚁𝚅𝙴𝚁: 𝙷𝙴𝚁𝙾𝙺𝚄
 ✯ 𝙱𝚄𝙸𝙻𝙳 𝚂𝚃𝙰𝚃𝚄𝚂: v1.0.1 [ 𝙱𝙴𝚃𝙰 ]"""
     SOURCE_TXT = """<b>NOTE:</b>
-- Eva Maria is a open source project. 
+- Ali is a open source project. 
 - Source - https://github.com/Rithin2277/EvaMaria  
 
 <b>DEVS:</b>
 - <a href=https://t.me/TeamEvamaria>Team Eva Maria</a>"""
     MANUELFILTER_TXT = """Help: <b>Filters</b>
 
-- Filter is the feature were users can set automated replies for a particular keyword and EvaMaria will respond whenever a keyword is found the message
+- Filter is the feature were users can set automated replies for a particular keyword and Ali will respond whenever a keyword is found the message
 
 <b>NOTE:</b>
 1. Ali should have admin privillage.
