5a8608e82e6d1878be61ad390d3b5f3c16505008
Noobcoder2728 <92325114+Noobcoder2728@users.noreply.github.com>
2021-12-05 15:49:43 +0530

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -38,7 +38,7 @@ async def save_group(bot, message):
             return
         buttons = [[
             InlineKeyboardButton('ℹ️ Help', url=f"https://t.me/{temp.U_NAME}?start=help"),
-            InlineKeyboardButton('📢 Updates', url='https://t.me/TeamEvamaria')
+            InlineKeyboardButton('📽 Join Movie Group', url='https://t.me/+dRjlw3eUuCk3NDQ9')
         ]]
         reply_markup=InlineKeyboardMarkup(buttons)
         await message.reply_text(
