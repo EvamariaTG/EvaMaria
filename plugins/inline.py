ef4a62167308f2aa73951c9584a6dec2e90fcbef
votrap <90063690+votrap@users.noreply.github.com>
2021-12-22 18:30:57 +0300

    empty string is better than None

--- plugins/inline.py
+++ plugins/inline.py
@@ -43,7 +43,7 @@ async def answer(bot, query):
         f_caption=file.caption
         if CUSTOM_FILE_CAPTION:
             try:
-                f_caption=CUSTOM_FILE_CAPTION.format(file_name=title, file_size=size, file_caption=f_caption)
+                f_caption=CUSTOM_FILE_CAPTION.format(file_name= '' if title is None else title, file_size='' if size is None else size, file_caption='' if f_caption is None else f_caption)
             except Exception as e:
                 logger.exception(e)
                 f_caption=f_caption
