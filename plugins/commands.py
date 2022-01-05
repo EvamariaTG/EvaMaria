dddbe11471147a833a612cdb6c54787a47dd0a7d
Jikku <66976170+jikkubot@users.noreply.github.com>
2022-01-05 11:52:11 +0400

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -71,7 +71,7 @@ async def start(client, message):
             btn.append([InlineKeyboardButton(" 🔄 Try Again", callback_data=f"checksub#{message.command[1]}")])
         await client.send_message(
             chat_id=message.from_user.id,
-            text="**Please Join My Updates Channel to use this Bot!**",
+            text=Script.FORCESUB_TXT,
             reply_markup=InlineKeyboardMarkup(btn),
             parse_mode="markdown"
             )
