56a83e539e6431813cfa65d851c6fa56adf638cf
Jikku <66976170+jikkubot@users.noreply.github.com>
2022-01-05 10:56:29 +0400

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -119,6 +119,7 @@ async def start(client, message):
         chat_id=message.from_user.id,
         file_id=file_id,
         caption=f_caption,
+        reply_markup=InlineKeyboardMarkup(buttons)
         )
                     
 
