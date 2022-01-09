be9e950c04f6c53778c3222aa1e2493b2290f501
CHINMay101 <87486492+CHINMay101@users.noreply.github.com>
2022-01-09 18:19:45 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -12,7 +12,7 @@ class script(object):
 ✯ 𝙱𝚄𝙸𝙻𝙳 𝚂𝚃𝙰𝚃𝚄𝚂: v1.0.1 [ 𝙱𝙴𝚃𝙰 ]"""
     SOURCE_TXT = """<b>NOTE:</b>
 - Eva Maria is a open source project. 
-- Source - https://github.com/EvamariaTG/EvaMaria  
+- Source - https://t.me/malluaudio 
 
 <b>DEVS:</b>
 - <a href=https://t.me/TeamEvamaria>Team Eva Maria</a>"""
