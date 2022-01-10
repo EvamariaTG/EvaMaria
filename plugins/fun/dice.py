2007e98c5a2354c174950518a364ae3b223bf46c
PaulWalker <91833744+saminsumesh@users.noreply.github.com>
2022-01-10 12:35:46 +0530

    Update dice.py

--- plugins/fun/dice.py
+++ plugins/fun/dice.py
@@ -3,14 +3,20 @@
  EMOJI = "🎲" #use the emoji which supports animation
  
  
- @Client.on_message(filters.command("roll", "dice")) #change the command according to your emoji name
- async def roll_dice(bot, message):
-     rep_msg_id = message.message.id
-     if message.reply_to_message:
-         rep_msg_id = message.reply_to_message.message.id
-     await bot.send_dice(
-     chat.id=message.chat.id,
-     emoji=EMOJI,
-     disable_web_page_preview=True,
-     reply_to_message_id=rep_msg_id
-     )
+from pyrogram import Client, filters
+ 
+EMOJI = "🎲" #use the emoji which supports animation
+ 
+ 
+@Client.on_message(
+    filters.command(["roll", "dice"]))
+async def roll_dice(client, message):
+    rep_mesg_id = message.message_id
+    if message.reply_to_message:
+        rep_mesg_id = message.reply_to_message.message_id
+    await client.send_dice(
+        chat_id=message.chat.id,
+        emoji=EMOJI,
+        disable_notification=True,
+        reply_to_message_id=rep_mesg_id
+    )
