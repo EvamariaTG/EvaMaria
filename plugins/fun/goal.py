34fec51fdf5a5398247de095e5a294ae10ed0b80
PaulWalker <91833744+saminsumesh@users.noreply.github.com>
2022-01-10 12:24:05 +0530

    initial push

---
+++ plugins/fun/goal.py
@@ -0,0 +1,16 @@
+ from pyrogram import Client, filters
+ 
+ EMOJI = "⚽" #use the emoji which supports animation
+ 
+ 
+ @Client.on_message(filters.command("goal", "shoot")) #change the command according to your emoji name
+ async def roll_dice(bot, message):
+     rep_msg_id = message.message.id
+     if message.reply_to_message:
+         rep_msg_id = message.reply_to_message.message.id
+     await bot.send_dice(
+     chat.id=message.chat.id,
+     emoji=EMOJI,
+     disable_web_page_preview=True,
+     reply_to_message_id=rep_msg_id
+     )
