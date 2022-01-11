18c757c2947d1e1ea65ef2d7175c1c03c112f152
Hash Minner <86665964+kalanakt@users.noreply.github.com>
2022-01-12 02:23:11 +0530

    fixed cap:

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -645,7 +645,11 @@ async def auto_filter(client, msg, spoll=False):
         cap = f"Here is what i found for your query {search}"
     if imdb and imdb.get('poster'):
         try:
-            await message.reply_photo(photo=imdb.get('poster'), caption=cap, reply_markup=InlineKeyboardMarkup(btn))
+            if len(cap) > 1024:
+               cap = f"Here is what i found for your Request {search}"
+               await message.reply_text(cap, reply_markup=InlineKeyboardMarkup(btn))
+            else:
+               await message.reply_photo(photo=imdb.get('poster'), caption=cap, reply_markup=InlineKeyboardMarkup(btn))
         except (MediaEmpty, PhotoInvalidDimensions, WebpageMediaEmpty):
             pic = imdb.get('poster')
             poster = pic.replace('.jpg', "._V1_UX360.jpg")
