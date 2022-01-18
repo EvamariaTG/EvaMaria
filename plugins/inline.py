b92d4cc94f6bc12df6c74ba2a21bdfedb5209344
soumadip <84225810+soymadip@users.noreply.github.com>
2022-01-19 02:16:48 +0530

    Fix at AUTH_USERS function
    The function was not working before, now after this fix it's working.

--- plugins/inline.py
+++ plugins/inline.py
@@ -16,7 +16,7 @@ async def inline_users(query: InlineQuery):
         return True
     return False
 
-@Client.on_inline_query()
+@Client.on_inline_query(filters.user(AUTH_USERS) if AUTH_USERS else None)
 async def answer(bot, query):
     """Show search results for given inline query"""
     
