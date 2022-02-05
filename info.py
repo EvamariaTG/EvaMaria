c0ed531b218d2da95b03abb9d5ecddcacef6e08e
JAGANVINOD <91742504+JAGANVINOD@users.noreply.github.com>
2022-02-05 17:18:10 +0530

    Update info.py

--- info.py
+++ info.py
@@ -53,6 +53,7 @@ FILE_STORE_CHANNEL = [int(ch) for ch in (environ.get('FILE_STORE_CHANNEL', '')).
 MELCOW_NEW_USERS = is_enabled((environ.get('MELCOW_NEW_USERS', "True")), True)
 PROTECT_CONTENT = is_enabled((environ.get('PROTECT_CONTENT', "False")), False)
 PUBLIC_FILE_STORE = is_enabled((environ.get('PUBLIC_FILE_STORE', "True")), True)
+UPSTREAM_REPO = environ.get("UPSTREAM_REPO", "https://github.com/JAGANVINOD/EvaMaria")
 
 LOG_STR = "Current Cusomized Configurations are:-\n"
 LOG_STR += ("IMDB Results are enabled, Bot will be showing imdb details for you queries.\n" if IMDB else "IMBD Results are disabled.\n")
