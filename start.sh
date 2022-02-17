7ca5868863aa62d79f18b3c79dc1d480a67f108c
calledo <96944972+calledo@users.noreply.github.com>
2022-02-17 08:12:12 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /EvaMaria
 fi
-cd /EvaMaria
+cd /calledogaming 
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
