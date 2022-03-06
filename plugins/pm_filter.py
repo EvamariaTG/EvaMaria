418a2a0585c5b54f57a109521f69fa882cde1678
Magic-Art-Room <99250685+Magic-Art-Room@users.noreply.github.com>
2022-03-06 13:16:01 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -708,7 +708,7 @@ async def auto_filter(client, msg, spoll=False):
             **locals()
         )
     else:
-        cap = f"<b>🎬 Title:</b> {search}\n</b>\n<b><a href='https://t.me/+cafbHw4riucxOWM1'>© Bot Managed By ArkBotz</a></b>\n\n<b> Note:</b> <i>"Here is what i found for your query {search}".</i>"
+        cap = f"Here is what i found for your query {search}"
     if imdb and imdb.get('poster'):
         try:
             await message.reply_photo(photo=imdb.get('poster'), caption=cap[:1024],
