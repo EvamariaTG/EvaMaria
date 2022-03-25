ad1c6e05cfcd4876a7727c6cc3206509fde48303
illuzX <98640218+illuzX@users.noreply.github.com>
2022-03-26 02:33:42 +0530

    update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -74,7 +74,7 @@ async def answer(bot, query):
                 reply_markup=reply_markup))
 
     if results:
-        switch_pm_text = f"{emoji.FILE_FOLDER} Results
+        switch_pm_text = f"{emoji.FILE_FOLDER} Results "
         if string:
             switch_pm_text += f" for {string}"
         try:
