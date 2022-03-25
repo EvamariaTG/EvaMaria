277217a75c34647385c97204e49d472bdd4fcc88
illuzX <98640218+illuzX@users.noreply.github.com>
2022-03-26 02:47:26 +0530

    update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -44,7 +44,7 @@ async def next_page(bot, query):
         return await query.answer("Bruh pleaseask you're request don't lazzy", show_alert=True)
 async def BUTTONS(bot, query):
     ident, req, key, offset = query.data.split('_')
-    if int(req) not in [query.from_user.id, 0]
+    if int(req) not in [query.from_user.id, 0]:
         await query.answer("Its not for u", show_alert=True)
     try:
         offset = int(offset)
