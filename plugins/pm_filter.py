09ace75398cf894bc14ebf4876a857697b807173
illuzX <98640218+illuzX@users.noreply.github.com>
2022-03-26 15:33:09 +0530

    update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -36,16 +36,12 @@ async def give_filter(client, message):
     if k == False:
         await auto_filter(client, message)
 
-
 @Client.on_callback_query(filters.regex(r"^next"))
 async def next_page(bot, query):
     ident, req, key, offset = query.data.split("_")
     if int(req) not in [query.from_user.id, 0]:
         return await query.answer("Bruh pleaseask you're request don't lazzy", show_alert=True)
-async def BUTTONS(bot, query):
-    ident, req, key, offset = query.data.split('_')
-    if int(req) not in [query.from_user.id, 0]:
-        await query.answer("Its not for u", show_alert=True)
+
     try:
         offset = int(offset)
     except:
