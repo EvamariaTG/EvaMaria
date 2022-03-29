d23be9c88b8b577a1650ea053952a4356d3fd56e
Leonardo Da Vinci <84631921+leanardo7994@users.noreply.github.com>
2022-03-29 09:34:39 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -772,7 +772,7 @@ async def advantage_spell_chok(msg):
         )
     ] for k, movie in enumerate(movielist)]
     btn.append([InlineKeyboardButton(text="Close", callback_data=f'spolling#{user}#close_spellcheck')])
-    await msg.reply("Hey {uso} I Cant Find Anything Related To Your Search\nDid You Mean Any One Of These? 👇",
+    await msg.reply("I Cant Find Anything Related To Your Search\nDid You Mean Any One Of These? 👇",
                     reply_markup=InlineKeyboardMarkup(btn))
 
 
