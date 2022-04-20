ed3edbe5ca14c4f871e010e890373802819fe6c6
KUNJAPPAN <69103277+hopepsycho@users.noreply.github.com>
2022-04-20 13:35:01 +0530

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -17,7 +17,7 @@ async def answer(bot, query):
     if AUTH_CHANNEL and not await is_subscribed(bot, query):
         await query.answer(results=[],
                            cache_time=0,
-                           switch_pm_text='You have to subscribe my channel to use the bot',
+                           switch_pm_text='You have to subscribe my updates to use the bot',
                            switch_pm_parameter="subscribe")
         return
 
