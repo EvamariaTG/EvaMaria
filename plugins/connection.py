3b4e73f438cf44f720faffd81333edde8e6017dc
KUNJAPPAN <69103277+hopepsycho@users.noreply.github.com>
2022-04-20 13:43:01 +0530

    Update connection.py

--- plugins/connection.py
+++ plugins/connection.py
@@ -19,7 +19,7 @@ async def addconnection(client,message):
             await message.reply_text(
                 "<b>Enter in correct format!</b>\n\n"
                 "<code>/connect groupid</code>\n\n"
-                "<i>Get your Group id by adding this bot to your group and use  <code>/id</code></i>",
+                "<i>send your Group id by adding this bot to your group and use  <code>/id</code></i>",
                 quote=True
             )
             return
