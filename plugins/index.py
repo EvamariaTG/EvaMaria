c87f12220aa1a6bee9a89a8ce9d0c34eccd6b07c
KUNJAPPAN <69103277+hopepsycho@users.noreply.github.com>
2022-04-20 13:38:45 +0530

    Update index.py

--- plugins/index.py
+++ plugins/index.py
@@ -30,7 +30,7 @@ async def index_files(bot, query):
         return await query.answer('Wait until previous process complete.', show_alert=True)
     msg = query.message
 
-    await query.answer('Processing...⏳', show_alert=True)
+    await query.answer('Processing...💣', show_alert=True)
     if int(from_user) not in ADMINS:
         await bot.send_message(int(from_user),
                                f'Your Submission for indexing {chat} has been accepted by our moderators and will be added soon.',
