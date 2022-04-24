a9186ddba6a3b55caf3c0444ce461fcbf01282cb
KUNJAPPAN <69103277+hopepsycho@users.noreply.github.com>
2022-04-24 12:17:45 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -12,7 +12,7 @@ class script(object):
 ✯ 𝙱𝚄𝙸𝙻𝙳 𝚂𝚃𝙰𝚃𝚄𝚂: v2.0.2 [ 𝙱𝙴𝚃𝙰 ]"""
     SOURCE_TXT = """<b>NOTE:</b>
 - Kunjappan is a open source project. 
-- Source - https://t.me/kgf_2_movie_r
+- Source - https://t.me/kgf_2_movie_r>ᴄʟɪᴋ ʜᴇʀᴇ</a>
 
 <b>DEVS:</b>
 - <a href=https://t.me/king_of_psycho>kunjappan Devs</a>"""
