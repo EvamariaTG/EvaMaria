6ee5f44b194cc446ea9c54716c460f261e04e01a
Leonardo Da Vinci <84631921+leanardo7994@users.noreply.github.com>
2022-05-02 19:25:01 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/EvamariaTG/EvaMaria.git /EvaMaria
+  git clone https://github.com/leanardo7994/File-Sharing-Bot.git /File-Sharing-Bot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /EvaMaria
+  git clone $UPSTREAM_REPO /File-Sharing-Bot
 fi
-cd /EvaMaria
+cd /File-Sharing-Bot
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
