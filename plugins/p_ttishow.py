caea3c0970e705d7732d9a65964359750fb5e84b
KUNJAPPAN <69103277+hopepsycho@users.noreply.github.com>
2022-05-03 11:53:03 +0530

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -51,7 +51,7 @@ async def save_group(bot, message):
                     await (temp.MELCOW['welcome']).delete()
                 except:
                     pass
-            temp.MELCOW['welcome'] = await message.reply(f"<b>💜 Hᴇʏ , {u.mention}, Wᴇʟᴄᴏᴍᴇ Tᴏ  {message.chat.title}</b>")
+            temp.MELCOW['welcome'] = await message.reply(f"<b>😍 Hᴇʏ , {u.mention}, Wᴇʟᴄᴏᴍᴇ Tᴏ  {message.chat.title}</b>")
 
 
 @Client.on_message(filters.command('leave') & filters.user(ADMINS))
