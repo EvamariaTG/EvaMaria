adbf0f5f2c2bf12ceef95c070a1ace40fa3fa26c
MKN TG <86054899+MrMKN@users.noreply.github.com>
2022-06-05 11:47:06 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -253,6 +253,7 @@ async def start(client, message):
         file_id=file_id,
         caption=f_caption,
         protect_content=True if pre == 'filep' else False,
+        reply_markup=BTN
         )
                     
 
