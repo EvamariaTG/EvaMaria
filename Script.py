d886b2454447652cd3dfc5052a07a388596f4db0
Gaurav Sulaniya <neodev268@gmail.com>
2022-06-06 19:41:52 +0530

    Create Script.py

--- Script.py
+++ Script.py
@@ -15,7 +15,7 @@ class script(object):
 - Source - https://github.com/realgaurv/autofilterbot 
 
 <b>DEVS:</b>
-- <a href=https://t.me/TeamEvamaria>Team Eva Maria</a>"""
+- <a href=https://t.me/RelaxroomBkp>Team Relaxroom</a>"""
     MANUELFILTER_TXT = """Help: <b>Filters</b>
 
 - Filter is the feature were users can set automated replies for a particular keyword and EvaMaria will respond whenever a keyword is found the message
