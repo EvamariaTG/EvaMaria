05d63e80486d077599e4043ed0e709c81cd57e43
Gaurav Sulaniya <neodev268@gmail.com>
2022-06-07 20:23:49 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -16,19 +16,4 @@ class script(object):
 
 <b>DEVS:</b>
 - <a href=https://t.me/RelaxroomBkp>Team Relaxroom</a>"""
-    MANUELFILTER_TXT = """Help: <b>Filters</b>
-    
-    STATUS_TXT = """★ 𝚃𝙾𝚃𝙰𝙻 𝙵𝙸𝙻𝙴𝚂: <code>{}</code>
-★ 𝚃𝙾𝚃𝙰𝙻 𝚄𝚂𝙴𝚁𝚂: <code>{}</code>
-★ 𝚃𝙾𝚃𝙰𝙻 𝙲𝙷𝙰𝚃𝚂: <code>{}</code>
-★ 𝚄𝚂𝙴𝙳 𝚂𝚃𝙾𝚁𝙰𝙶𝙴: <code>{}</code> 𝙼𝚒𝙱
-★ 𝙵𝚁𝙴𝙴 𝚂𝚃𝙾𝚁𝙰𝙶𝙴: <code>{}</code> 𝙼𝚒𝙱"""
-    LOG_TEXT_G = """#NewGroup
-Group = {}(<code>{}</code>)
-Total Members = <code>{}</code>
-Added By - {}
-"""
-    LOG_TEXT_P = """#NewUser
-ID - <code>{}</code>
-Name - {}
-"""
+   
