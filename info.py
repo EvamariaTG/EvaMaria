a14e33640137d5e603e017b460a8963ea3bf4125
MKN TG <86054899+MrMKN@users.noreply.github.com>
2022-06-10 18:31:32 +0530

    Update info.py

--- info.py
+++ info.py
@@ -36,6 +36,11 @@ DATABASE_URI = environ.get('DATABASE_URI', "")
 DATABASE_NAME = environ.get('DATABASE_NAME', "Rajappan")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
+#extra by @MrMKN
+BTN_ALERT = environ.get("BTN_ALERT", "⚠️ 𝙃𝙚𝙮, {user}! 𝙏𝙝𝙖𝙩'𝙨 𝙉𝙤𝙩 𝙁𝙤𝙧 𝙔𝙤𝙪. 𝙋𝙡𝙚𝙖𝙨𝙚 𝙍𝙚𝙦𝙪𝙚𝙨𝙩 𝙔𝙤𝙪𝙧 𝙊𝙬𝙣")
+ALERT_1 = environ.get("ALERT_1", "No Such files 😔")
+
+
 # Others
 FORCE_TEXT = environ.get('FORCE_TEXT', '♦️ READ THIS INSTRUCTION ♦️\n🗣 നിങ്ങൾ ചോദിക്കുന്ന സിനിമകൾ നിങ്ങൾക്ക് ലഭിക്കണം എന്നുണ്ടെങ്കിൽ നിങ്ങൾ താഴെ കൊടുത്തിട്ടുള്ള ചാനലിൽ ജോയിൻ ചെയ്യണം ജോയിൻ ചെയ്ത ശേഷം വീണ്ടും ഗ്രൂപ്പിൽ പോയി ആ ബട്ടനിൽ അമർത്തിയാൽ നിങ്ങൾക്ക് ഞാൻ ആ സിനിമ പ്രൈവറ്റ് ആയി അയച്ചു തരുന്നതാണ് 😍\n🗣 In Order To Get The Movie Requested By You in Our Groups You Will Have To Join Our Official Channel First After That Try Accessing That Movie Again From Our Group Ill Send You That Movie Privately 🙈\n👇 JOIN THIS CHANNEL And TRY 👇')
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', 0))
