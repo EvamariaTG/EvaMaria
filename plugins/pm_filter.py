e4b4c55152e125c98ad247b4edf4176f3e59b7b6
Jins Mathew <jins.palakkattu@gmail.com>
2022-06-20 09:05:29 +0300

    Bug Fix On Double Filter Button

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -655,7 +655,7 @@ async def auto_filter(client, msg, spoll=False):
                 ),
                 InlineKeyboardButton(
                     text=f"{get_size(file.file_size)}",
-                    callback_data=f'{pre}_#{file.file_id}',
+                    callback_data=f'{pre}#{file.file_id}',
                 ),
             ]
             for file in files
