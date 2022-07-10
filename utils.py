ac5544810405bae63fb3abc5854740bcd3435151
KUNJAPPAN <69103277+hopepsycho@users.noreply.github.com>
2022-07-10 19:35:09 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 import logging
 from pyrogram.errors import InputUserDeactivated, UserNotParticipant, FloodWait, UserIsBlocked, PeerIdInvalid
 from info import AUTH_CHANNEL, LONG_IMDB_DESCRIPTION, MAX_LIST_ELM
+from imdb import IMDb
 import asyncio
 from pyrogram.types import Message
 from typing import Union
