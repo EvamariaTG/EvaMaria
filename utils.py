e171c787b523679e7205b253a7922eb9843c92d1
KUNJAPPAN <69103277+hopepsycho@users.noreply.github.com>
2022-07-10 23:51:25 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -21,7 +21,7 @@ BTN_URL_REGEX = re.compile(
     r"(\[([^\[]+?)\]\((buttonurl|buttonalert):(?:/{0,2})(.+?)(:same)?\))"
 )
 
-IMDbpy==2021.4.18 
+IMDb = imdbpy==2021.4.18 
 
 BANNED = {}
 SMART_OPEN = '“'
