e474a0abbea96815ea0096c02b1602c9866e090b
yohanrock <77877340+yohanrock@users.noreply.github.com>
2022-07-27 17:29:51 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -45,7 +45,8 @@ async def start(client, message):
             InlineKeyboardButton('➕ Add Me To Your Groups ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
             ],[
             InlineKeyboardButton('🔍 Search', switch_inline_query_current_chat=''),
-            InlineKeyboardButton('🤖 Updates', url='https://t.me/vaarasudu_vt')
+            InlineKeyboardButton('🤖 Updates', url='https://t.me/vaarasudu_vt'), 
+            InlineKeyboardButton('📌 Support Chart', url='https://t.me/infinitymoviesgroup')
             ],[
             InlineKeyboardButton('ℹ️ Help', callback_data='help'),
             InlineKeyboardButton('😊 About', callback_data='about')
@@ -91,7 +92,8 @@ async def start(client, message):
             InlineKeyboardButton('➕ Add Me To Your Groups ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
             ],[
             InlineKeyboardButton('🔍 Search', switch_inline_query_current_chat=''),
-            InlineKeyboardButton('🤖 Updates', url='https://t.me/vaarasudu_vt')
+            InlineKeyboardButton('🤖 Updates', url='https://t.me/vaarasudu_vt'), 
+            InlineKeyboardButton('📌 Support Chart', url='https://t.me/infinitymoviesgroup') 
             ],[
             InlineKeyboardButton('ℹ️ Help', callback_data='help'),
             InlineKeyboardButton('😊 About', callback_data='about')
