a45eb0ada803dd17626077e7cb0066192f4c09bb
Akbotmakersnew <92969505+Akbotmakersnew@users.noreply.github.com>
2022-07-28 09:27:30 +0530

    Update info.py

--- info.py
+++ info.py
@@ -37,6 +37,7 @@ DATABASE_NAME = environ.get('DATABASE_NAME', "Rajappan")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Others
+DELETE_TIME = int(environ.get('DELETE_TIME', 300))
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', 0))
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'TeamEvamaria')
 P_TTI_SHOW_OFF = is_enabled((environ.get('P_TTI_SHOW_OFF', "False")), False)
