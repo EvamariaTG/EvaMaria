e05cd9dfc2a97e1bfb33e350010e4acaa56da632
mastermindmayank <88104889+mastermindmayankofficial@users.noreply.github.com>
2022-08-22 19:29:32 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -44,7 +44,7 @@ async def start(client, message):
             InlineKeyboardButton('➕ Add Me To Your Groups ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
             ],[
             InlineKeyboardButton('🔍 Search', switch_inline_query_current_chat=''),
-            InlineKeyboardButton('🤖 Updates', url='https://t.me/Masterminayankproject')
+            InlineKeyboardButton('🤖 Updates', url='https://t.me/Mastermindmayankproject')
             ],[
             InlineKeyboardButton('ℹ️ Help', callback_data='help'),
             InlineKeyboardButton('😊 About', callback_data='about')
